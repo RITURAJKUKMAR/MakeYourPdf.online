7f7ea0fe09dd18a8ad794acf9251cdd477752cbe
MR.RITU-R <riturajkumar1105@gmail.com>
2025-10-25 11:19:36 +0530

    Add files via upload

---
+++ dragover.js
@@ -0,0 +1,39 @@
+let dropArea = document.getElementById('dropArea');
+let inputFile = document.getElementById('inputFile');
+
+dropArea.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    dropArea.style.borderColor = 'blue';
+});
+
+dropArea.addEventListener('dragleave', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    dropArea.style.borderColor = '#ccc';
+});
+
+dropArea.addEventListener('drop', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    dropArea.style.borderColor = '#ccc';
+
+    let files = e.dataTransfer.files;
+    let dataTransfer = new DataTransfer();
+    for (let i = 0; i < files.length; i++) {
+        dataTransfer.items.add(files[i]);
+    }
+    inputFile.files = dataTransfer.files;
+    showFileNames(files);
+});
+
+inputFile.addEventListener('change', () => {
+    showFileNames(inputFile.files);
+});
+
+let showFileNames = (files) => {
+    let text = '';
+    for (i = 0; i < files.length; i++)
+        text += files[i].name + '<br/>';
+    document.getElementById("filesNames").innerHTML = '<h2>Selected files:</h2>\n' + text;
+}
